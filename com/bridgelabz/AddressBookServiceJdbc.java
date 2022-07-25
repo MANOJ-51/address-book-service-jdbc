7041250b025b9df8db75f819f7e8b7e334551d28
MANOJ-51 <m4anoj51@gmail.com>
2022-07-25 15:24:13 +0530

    added: method to retrieve data

--- com/bridgelabz/AddressBookServiceJdbc.java
+++ com/bridgelabz/AddressBookServiceJdbc.java
@@ -1,8 +1,6 @@
 package com.bridgelabz;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class AddressBookServiceJdbc {
     public static void main(String[] args) {
@@ -12,6 +10,13 @@ public class AddressBookServiceJdbc {
             Connection connection = DriverManager.getConnection
                     ("jdbc:mysql://localhost:3306/address_book_service", "root", "Manoj@451");
             System.out.println("connection successful");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from address_book");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1) + "|" + resultSet.getString(2) + "|" + resultSet.getString(3) + "|" +
+                        resultSet.getString(4) + "|" + resultSet.getString(5) + "|" + resultSet.getInt(6) + "|" + resultSet.getInt(7) +
+                        "|" + resultSet.getString(8) + "|" + resultSet.getString(9));
+            }
             connection.close();
         } catch (SQLException exception) {
             exception.printStackTrace();
