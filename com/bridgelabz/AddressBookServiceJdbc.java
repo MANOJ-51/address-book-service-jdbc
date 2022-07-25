6c130925415b5bce16173db8589fdf5e39fbd125
MANOJ-51 <m4anoj51@gmail.com>
2022-07-25 15:58:04 +0530

    added: method to find the data by date range

--- com/bridgelabz/AddressBookServiceJdbc.java
+++ com/bridgelabz/AddressBookServiceJdbc.java
@@ -12,15 +12,19 @@ public class AddressBookServiceJdbc {
             System.out.println("connection successful");
             Statement statement = connection.createStatement();
             //updating contact
-            statement.executeUpdate("update address_book set first_name = 'navdeep' where phone_number=91213107");
+            // statement.executeUpdate("update address_book set first_name = 'navdeep' where phone_number=91213107");
+            //altering table adding start date
+            //statement.executeUpdate("alter table address_book add start_date date;");
+            //inserting date into column
+            //statement.executeUpdate("update address_book set start_date ='2020-01-02' where type = 'friends' or type = 'family'");
             //retrieve data
-            ResultSet resultSet = statement.executeQuery("select * from address_book");
+            //ResultSet resultSet = statement.executeQuery("select * from address_book");
+            ResultSet resultSet = statement.executeQuery("select * from address_book where start_date between cast('2020-01-01' as date) and date(now());");
             while (resultSet.next()) {
                 System.out.println(resultSet.getString(1) + "|" + resultSet.getString(2) + "|" + resultSet.getString(3) + "|" +
                         resultSet.getString(4) + "|" + resultSet.getString(5) + "|" + resultSet.getInt(6) + "|" + resultSet.getInt(7) +
-                        "|" + resultSet.getString(8) + "|" + resultSet.getString(9));
+                        "|" + resultSet.getString(8) + "|" + resultSet.getString(9) + "|" + resultSet.getDate(10));
             }
-
             connection.close();
         } catch (SQLException exception) {
             exception.printStackTrace();
