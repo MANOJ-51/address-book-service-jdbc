1f83fc0dea5291746366449fea80a6115f101244
MANOJ-51 <m4anoj51@gmail.com>
2022-07-25 15:04:12 +0530

    added: connection to data base

--- com/bridgelabz/AddressBookServiceJdbc.java
+++ com/bridgelabz/AddressBookServiceJdbc.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class AddressBookServiceJdbc {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Service Jdbc");
+
+        try {
+            Connection connection = DriverManager.getConnection
+                    ("jdbc:mysql://localhost:3306/address_book_service", "root", "Manoj@451");
+            System.out.println("connection successful");
+            connection.close();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 }
